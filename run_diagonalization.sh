37bfb058f926034716ba838e3c11ce373aff7c64
Jan Suntajs <jansuntajs@gmail.com>
2018-10-26 11:42:40 +0200

    Added running script

---
+++ run_diagonalization.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+#SBATCH --time=2:59:00
+#SBATCH --mem=4000
+#SBATCH -c 8
+
+source deactivate
+module purge
+module load intel
+module load intelpython2/2018-u1
+which python2.7>> my-log.log
+
+srun python2.7 python_diagonalization.py 0 13
+
+source activate
